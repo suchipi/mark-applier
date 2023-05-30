c8ab14a0599b11f47c62e62fb1a747e44571a1c4
Lily Skye <me@suchipi.com>
2023-05-29 23:41:49 -0600

    make --help test less fragile

--- tests/help.test.js
+++ tests/help.test.js
@@ -1,7 +1,10 @@
 import { rootDir, runCli, cleanResult } from "./test-utils";
+import helpText from "../dist/helptext";
 
 test("--help", async () => {
   const run = runCli(["--help"]);
   await run.completion;
-  expect(cleanResult(run.result)).toMatchSnapshot();
+  expect(run.result.code).toBe(0);
+  expect(run.result.stderr).toBe("");
+  expect(run.result.stdout).toBe(helpText + "\n");
 });
